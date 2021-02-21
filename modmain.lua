12111fab54484fb1ede4416562f7e85303cac683
jupitersh <suda0huan9@gmail.com>
2021-02-21 15:57:26 +0800

    修改检测物品的方法

--- modmain.lua
+++ modmain.lua
@@ -32,40 +32,16 @@ if GetModConfigData("mode") == 1 then
 
 else
 
-    local function AutoCheck(inst)
-        local player=GLOBAL.ThePlayer
+    AddPlayerPostInit(function(player)
         if player then
-            local invent=player.replica.inventory
-            local cont=nil
-            local inspect_back=false
-            if GLOBAL.EQUIPSLOTS.BACK then
-                cont=invent:GetEquippedItem(GLOBAL.EQUIPSLOTS.BACK)
-            else
-                cont=invent:GetEquippedItem(GLOBAL.EQUIPSLOTS.BODY)
-            end
-            if cont~=nil and cont.replica.container then
-                local container=cont.replica.container
-                for i=1,container:GetNumSlots() do
-                    local Item=container:GetItemInSlot(i)
-                    if Item then
-                        invent:InspectItemFromInvTile(Item)
-                        inspect_back=true
-                    end
+            player:DoPeriodicTask(8*60, function()
+                local inventory = player.replica.inventory
+                local item = inventory:GetItemInSlot(1)
+                if item then
+                    inventory:InspectItemFromInvTile(item)
                 end
-            end
-            if inspect_back==false then
-                for i=1,GLOBAL.GetMaxItemSlots(GLOBAL.TheNet:GetServerGameMode()) do
-                    local Item2=invent:GetItemInSlot(i)
-                    if Item2 then
-                        invent:InspectItemFromInvTile(Item2)
-                    end
-                end
-            end
+            end)
         end
-    end
-
-    AddPlayerPostInit(function(inst)
-        inst:DoPeriodicTask(480, AutoCheck)
     end)
 
 end
