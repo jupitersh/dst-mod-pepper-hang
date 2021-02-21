3cf1c2efc0bb45b88a5ae2b350e00311737021c4
jupitersh <suda0huan9@gmail.com>
2021-02-21 18:08:26 +0800

    添加检测身边物品

--- modinfo.lua
+++ modinfo.lua
@@ -4,7 +4,7 @@ description = [[
 防止因长期没有向服务器放送数据而掉线
 ]]
 author = "辣椒小皇纸"
-version = "1.2.2"
+version = "1.3.0"
 forumthread = ""
 api_version = 10
 
@@ -16,3 +16,21 @@ client_only_mod = true
 all_clients_require_mod = false
 
 priority = -99
+
+----------------------
+-- General settings --
+----------------------
+
+configuration_options =
+{
+    {
+        name = "mode",
+        label = "模式",
+        hover = "",
+        options =   {
+                        {description = "检测身边的物品", data = 1, hover = ""},
+                        {description = "检测身上的物品", data = 2, hover = ""},
+                    },
+        default = 1,
+    },
+}
--- modmain.lua
+++ modmain.lua
@@ -1,14 +1,40 @@
-AddPlayerPostInit(function(player)
-    if player then
-        player:DoPeriodicTask(8*60, function()
-            local inventory = player.replica.inventory
-            for i = 1, 15 do
-                local item = inventory:GetItemInSlot(i)
-                if item then
-                    inventory:InspectItemFromInvTile(item)
-                end
-                return
+if GetModConfigData("mode") == 1 then
+
+    local function FindClosestInst(player)
+        local x,y,z = player.Transform:GetWorldPosition()
+        local ents = GLOBAL.TheSim:FindEntities(x, y, z, 20, nil, {"player", "FX", "NOCLICK", "DECOR", "INLIMBO", "CLASSIFIED"}, nil)
+        local closest = nil
+        local closeness = nil
+        for k,v in pairs(ents) do
+            if closest == nil or player:GetDistanceSqToInst(v) < closeness then
+                closest = v
+                closeness = player:GetDistanceSqToInst(v)
             end
-        end)
+        end
+        return closest
     end
-end)
+
+    AddPlayerPostInit(function(player)
+        player:DoPeriodicTask(8*60, function(player)
+            local ent = FindClosestInst(player)
+            local x,y,z = ent.Transform:GetWorldPosition()
+            GLOBAL.SendRPCToServer(25, 87, x, z, ent, false, 1, nil, nil, nil, false)
+        end)
+    end)
+
+else
+    AddPlayerPostInit(function(player)
+        if player then
+            player:DoPeriodicTask(8*60, function()
+                local inventory = player.replica.inventory
+                for i = 1, 15 do
+                    local item = inventory:GetItemInSlot(i)
+                    if item then
+                        inventory:InspectItemFromInvTile(item)
+                    end
+                    return
+                end
+            end)
+        end
+    end)
+end
